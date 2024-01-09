2ca35d0e660a3c8b5593eb20d529db8a640268e7
tinaguhwa23 <tinaguhwa@gmail.com>
2024-01-09 01:57:54 -0800

    betty will be proud

---
+++ betty.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - This is the entry point of the program
+ *
+ * Description: This is a simple program that prints "Hello, Simple Shell!"
+ * to the standard output.
+ *
+ * Return: Always returns 0 (Success)
+ */
+int main(void)
+{
+	printf("Hello, Simple Shell!\n");
+
+	return (0);
+}
